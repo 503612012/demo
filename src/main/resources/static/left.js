95d4434b7bc6dbff3758459d744a40c529af9bf8
zhangzq7 <zhangzq7@asiainfo.com>
2018-10-22 09:25:01 +0800

    Init

--- src/main/resources/static/left.js
+++ src/main/resources/static/left.js
@@ -8,19 +8,31 @@ $(function () {
         var html = '';
         for (var i = 0; i < data.length; i++) {
             html += '<li data-name="" data-jump="" class="layui-nav-item ' + (i == 0 ? 'layui-nav-itemed' : '') + '">';
-            html += '<a href="javascript:" lay-tips="主页" lay-direction="2">';
+            html += '<a href="javascript:" lay-tips="主页" lay-direction="2" class="level-one-menu">';
             html += '<i class="layui-icon layui-icon-home"></i>';
             html += '<cite>' + data[i].menu.menuName + '</cite>';
-            html += '<span class="layui-nav-more"></span>';
+            if (data[i].children.length > 0) {
+                html += '<span class="layui-nav-more"></span>';
+            }
             html += '</a>';
             for (var j = 0; j < data[i].children.length; j++) {
                 html += '<dl class="layui-nav-child">';
-                html += '<dd data-name="" data-jump="/"><a href="javascript:" lay-href="/">' + data[i].children[j].menuName + '</a></dd>';
+                html += '<dd data-name="" data-jump="/" class="level-two-menu"><a href="javascript:" lay-href="/">' + data[i].children[j].menuName + '</a></dd>';
                 html += '</dl>';
             }
             html += '</li>';
         }
-        $("#user-menus").html(html);
+        $("#user-menus").html(html).find(".level-one-menu").on("click", function() {
+            $(this).parent().toggleClass("layui-nav-itemed");
+            $(".menu-border-left").removeClass("menu-border-left");
+            $(this).addClass("menu-border-left");
+        });
+        $("#user-menus").find(".level-two-menu").on("click", function() {
+            $(".layui-this").removeClass("layui-this");
+            $(this).addClass("layui-this");
+            $(".menu-border-left").removeClass("menu-border-left");
+            $(this).parent().parent().find(".level-one-menu").addClass("menu-border-left");
+        });
     };
 
     /**
