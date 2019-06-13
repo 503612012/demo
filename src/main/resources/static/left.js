3992cb3d4372bfa9a0718147484f67eb018c5502
zhangzq7 <zhangzq7@asiainfo.com>
2019-06-13 11:32:17 +0800

    左侧边栏缩回和弹出

--- src/main/resources/static/left.js
+++ src/main/resources/static/left.js
@@ -44,7 +44,7 @@ $(function() {
         data: {},
         dataType: "json",
         success: function(result) {
-            if (result.code != 200) {
+            if (result.code !== 200) {
                 layer.open({
                     title: '系统提示',
                     content: result.data,
@@ -53,6 +53,54 @@ $(function() {
                 return;
             }
             fillMenus(result.data);
+        }
+    });
+
+    var isShow = true;  // 定义一个标志位
+    // 点击一级标题的时候打开缩回去的边栏
+    $("body").on("click", ".level-one-menu", function() {
+        if (!isShow) {
+            $('.kit-side-fold').click();
+        }
+    });
+
+    $('.kit-side-fold').click(function() {
+        // 选择出所有的span，并判断是不是hidden
+        $('.layui-nav-item span').each(function() {
+            if ($(this).is(':hidden')) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+        // 判断isshow的状态
+        if (isShow) { // 缩回去
+            $('.layui-side.layui-bg-black').animate({'width': '55px'}); // 设置宽度
+            // $('.kit-side-fold i').animate({'margin-right': '70%'});  // 修改图标的位置
+            // 将footer和body的宽度修改
+            $('.layui-body').animate({'left': '55px'});
+            $('.layui-footer').animate({'left': '55px'});
+            // 将二级导航栏隐藏
+            $('dd span').each(function() {
+                $(this).hide();
+            });
+            // 修改标志位
+            // $('body').find(".layui-nav-itemed").removeClass("layui-nav-iteme");
+            $('body').find(".layui-icon-shrink-right").removeClass("layui-icon-shrink-right").addClass("layui-icon-spread-left");
+            $('body').find("li.layui-nav-itemed a.level-one-menu").click();
+            $('body').find("a.level-one-menu cite").addClass("hide");
+            isShow = false;
+        } else { // 展开
+            $('.layui-side.layui-bg-black').animate({'width': '200px'});
+            // $('.kit-side-fold i').animate({'margin-right': '10%'});
+            $('.layui-body').animate({'left': '200px'});
+            $('.layui-footer').animate({'left': '200px'});
+            $('dd span').each(function() {
+                $(this).show();
+            });
+            $('body').find(".layui-icon-spread-left").removeClass("layui-icon-spread-left").addClass("layui-icon-shrink-right");
+            $('body').find("a.level-one-menu cite").removeClass("hide");
+            isShow = true;
         }
     });
 
