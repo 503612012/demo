a7346d8e7d14f91bff9fe4c15e27c99283b79d82
zhangzq7 <zhangzq7@asiainfo.com>
2020-11-06 11:09:45 +0800

    日常改造

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-env_args="-Xms128M -Xmx128M -XX:NewRatio=2 -Xverify:none -Dloader.path=./target,./target/classes,./target/lib"
+env_args="-Xms64M -Xmx64M -XX:NewRatio=2 -Xverify:none -Dloader.path=./target,./target/classes,./target/lib"
 APP_NAME="`find ./target -name 'demo*.jar'`"
 pid=`ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}'`
 if [[ "$pid" != "" ]]
