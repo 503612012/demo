5b70fa52b81aafca2b79690db1a6c723e29b8912
zhangzq7 <zhangzq7@asiainfo.com>
2021-04-23 08:58:20 +0800

    init

---
+++ src/main/bin/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+env_args="-Xms512M -Xmx512M -XX:NewRatio=2 -Xverify:none -Dloader.path=./target/,./target/classes,./target/lib"
+APP_NAME="`find ./target/ -name 'diary-new*.jar'`"
+pid=`ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}'`
+if [[ "$pid" != "" ]]
+then
+    echo "${APP_NAME} already startup!"
+else
+    nohup java $env_args -jar ${APP_NAME} > /dev/null 2>&1 &
+fi
