60d5fd9da342eb279161caa93d559b8fd40d4ab8
zhangzq7 <zhangzq7@asiainfo.com>
2021-12-10 14:38:33 +0800

    init

--- src/main/bin/app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /data/docker/demo
-sh start.sh
-tail -f /data/docker/demo/resources/application.properties
--- src/main/bin/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-APP_NAME="`find /data/docker/demo/ -name 'demo*.jar'`"
-pid=`ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}'`
-if [[ "$pid" != "" ]]
-then
-    kill -9 ${pid}
-else
-    echo "${APP_NAME} already stop!"
-fi
