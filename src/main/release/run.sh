f542188381db137b5781181df9688d36079bba36
zhangzq7 <zhangzq7@asiainfo.com>
2022-03-03 17:00:30 +0800

    init

--- src/main/release/run.sh
+++ src/main/release/run.sh
@@ -1,2 +1,3 @@
-mkdir /home/demo/logs
+if [[ ! -d "/home/demo/logs" ]]; then mkdir /home/demo/logs; fi
+if [[ ! -d "/home/demo/files" ]]; then mkdir /home/demo/files; fi
 docker run -d --name=demo -v /home/demo/logs:/home/demo/logs -p 45678:45678 demo:1.0.0
