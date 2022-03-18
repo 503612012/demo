0eda3c25df6e0590d4a13e5151f390495370b547
zhangzq7 <zhangzq7@asiainfo.com>
2022-03-18 17:43:38 +0800

    init

--- src/main/release/start.sh
+++ src/main/release/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-env_args="-Xms256M -Xmx256M -XX:NewRatio=2 -Xverify:none -Dloader.path=./,./lib,./resources"
-APP_NAME="`find /home/demo/ -name 'demo*.jar'`"
-pid=`ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}'`
-if [[ "$pid" != "" ]]
-then
-    echo "${APP_NAME} already startup!"
+env_args="-Xms8g -Xmx8g -XX:MaxDirectMemorySize=2048M -Dloader.path=./,./lib,./resources"
+#env_args="-Xms256M -Xmx256M -XX:MaxDirectMemorySize=2048M -Dloader.path=./,./lib,./resources"
+APP_NAME="$(find /home/demo/ -name 'demo*.jar')"
+pid=$(ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}')
+if [[ "$pid" != "" ]]; then
+  echo "${APP_NAME} already startup!"
 else
-    nohup java $env_args -jar ${APP_NAME} > /dev/null 2>&1 &
+  nohup java $env_args -jar ${APP_NAME} > /dev/null 2>&1 &
 fi
--- src/main/release/stop.sh
+++ src/main/release/stop.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-APP_NAME="`find /home/demo/ -name 'demo*.jar'`"
-pid=`ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}'`
-if [[ "$pid" != "" ]]
-then
-    kill -9 ${pid}
+APP_NAME="$(find /home/demo/ -name 'demo*.jar')"
+pid=$(ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}')
+if [[ "$pid" != "" ]]; then
+  kill -9 ${pid}
 else
-    echo "${APP_NAME} already stop!"
+  echo "${APP_NAME} already stop!"
 fi
