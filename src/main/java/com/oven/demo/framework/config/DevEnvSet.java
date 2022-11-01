e55bb049021efbc3f6e33fcaee0e2dcb045f6aed
zhangzq7 <zhangzq7@asiainfo.com>
2022-11-01 11:32:43 +0800

    init

--- src/main/java/com/oven/demo/framework/config/DevEnvSet.java
+++ src/main/java/com/oven/demo/framework/config/DevEnvSet.java
@@ -1,7 +1,13 @@
 package com.oven.demo.framework.config;
 
-import org.springframework.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Profile;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -9,27 +15,55 @@ import java.util.Properties;
  *
  * @author Oven
  */
+@Slf4j
 public class DevEnvSet {
+
+    /**
+     * Win
+     */
+    private static void configWithWin(Properties properties) {
+        properties.put("log.home", "E:\\logs\\demo\\");
+    }
+
+    /**
+     * Mac
+     */
+    private static void configWithMac(Properties properties) {
+        properties.put("log.home", "/Users/oven/logs/demo/logs/");
+    }
 
     /**
      * 开发环境根据自己操作系统和相关文件路径修改一下配置
      */
     public static void config(Properties properties) {
-        String profile = String.valueOf(properties.get("demo.profile"));
-        if (PropertyConfig.PRO_PROFILE.equals(profile)) {
-            return;
-        }
-        properties.put("logging.config", "classpath:logback-dev.xml");
+        try {
+            String profile = String.valueOf(properties.get("vcas.profile"));
+            if (PropertyConfig.PRO_PROFILE.equals(profile)) {
+                return;
+            }
+            log.info("=========================== >>> 开始初始化开发环境配置");
+            properties.put("logging.config", "classpath:logback-dev.xml");
 
-        String platform = System.getenv("p");
-        // Mac
-        if (StringUtils.isEmpty(platform) || "m".equalsIgnoreCase(platform)) {
-            properties.put("log.home", "/Users/oven/logs/demo/logs/");
-        }
-
-        // Win
-        if ("w".equalsIgnoreCase(platform)) {
-            properties.put("log.home", "E:\\logs\\demo\\");
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            String path = System.getProperty("user.dir") + File.separator + "pom.xml";
+            Model model = reader.read(new FileReader(path));
+            List<Profile> profiles = model.getProfiles();
+            for (Profile platform : profiles) {
+                if (!platform.getActivation().isActiveByDefault()) {
+                    continue;
+                }
+                // Mac
+                if (platform.getProperties().get("platform").toString().startsWith("m")) {
+                    configWithMac(properties);
+                }
+                // Win
+                if (platform.getProperties().get("platform").toString().startsWith("w")) {
+                    configWithWin(properties);
+                }
+            }
+            log.info("=========================== >>> 初始化开发环境配置完成");
+        } catch (Exception e) {
+            log.info("=========================== >>> 初始化开发环境配置异常：", e);
         }
     }
 
