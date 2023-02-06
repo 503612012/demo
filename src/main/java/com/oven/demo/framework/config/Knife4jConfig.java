bd617f5ef3fda05219f10fd03ccf63cb20f72b91
Oven <503612012@qq.com>
2023-02-06 08:57:23 +0800

    init

--- src/main/java/com/oven/demo/framework/config/Knife4jConfig.java
+++ src/main/java/com/oven/demo/framework/config/Knife4jConfig.java
@@ -1,6 +1,9 @@
 package com.oven.demo.framework.config;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -12,23 +15,28 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.io.File;
+import java.io.FileReader;
+
 /**
  * knife4j配置
  *
  * @author Oven
  */
+@Slf4j
 @EnableKnife4j
 @Configuration
 @EnableSwagger2
 public class Knife4jConfig {
 
     private ApiInfo apiInfo() {
+        String version = getVersion();
         return new ApiInfoBuilder()
                 .title("财务管理系统接口文档")
                 .description("财务管理系统接口文档")
                 .termsOfServiceUrl("http://localhost:8080")
                 .contact(new Contact("Oven", "http://cloveaire.com", "503612012@qq.com"))
-                .version("1.0.0")
+                .version(version)
                 .build();
     }
 
@@ -41,6 +49,18 @@ public class Knife4jConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.oven.demo"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    public String getVersion() {
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            String path = System.getProperty("user.dir") + File.separator + "pom.xml";
+            Model model = reader.read(new FileReader(path));
+            return model.getVersion();
+        } catch (Exception e) {
+            log.error("获取maven版本号异常：", e);
+            return null;
+        }
     }
 
 }
