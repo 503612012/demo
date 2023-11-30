906be6bb10fef6bc094c041b64749bd6a42300f1
Oven <503612012@qq.com>
2023-11-30 10:03:41 +0800

    init

--- src/main/release/run.sh
+++ src/main/release/run.sh
@@ -9,5 +9,5 @@ if [[ $count > 0 ]]; then
     docker start demo
   fi
 else
-  docker run -d --name=demo -v ${APP_HOME}/logs:/home/demo/logs -v /data/storage/demo:/data/storage -v ${APP_HOME}/license:${APP_HOME}/license -v /usr/sbin/dmidecode:/usr/sbin/dmidecode -v /dev/mem:/dev/mem --privileged=true -p 6004:45678 demo:@version@
+  docker run -d --name=demo -v ${APP_HOME}/logs:/home/demo/logs -v /data/storage/demo:/data/storage -v /usr/sbin/dmidecode:/usr/sbin/dmidecode -v /dev/mem:/dev/mem --privileged=true -p 6004:45678 demo:@version@
 fi
