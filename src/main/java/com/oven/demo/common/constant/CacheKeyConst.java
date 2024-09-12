0e82033d59bf98a22c3b3289c1b13a9ab1581890
Oven <503612012@qq.com>
2024-09-12 18:57:26 +0800

    init

---
+++ src/main/java/com/oven/demo/common/constant/CacheKeyConst.java
@@ -0,0 +1,7 @@
+package com.oven.demo.common.constant;
+
+public interface CacheKeyConst {
+
+    CharSequence GET_CRONTAB_BY_KEY = "get_crontab_by_key_{}";
+
+}
--- src/main/java/com/oven/demo/core/crontab/service/CrontabService.java
+++ src/main/java/com/oven/demo/core/crontab/service/CrontabService.java
@@ -1,5 +1,8 @@
 package com.oven.demo.core.crontab.service;
 
+import cn.hutool.core.util.StrUtil;
+import com.oven.basic.common.cache.CacheManager;
+import com.oven.demo.common.constant.CacheKeyConst;
 import com.oven.demo.core.crontab.dao.CrontabDao;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +23,17 @@ public class CrontabService {
      * 根据key获取cron表达式
      */
     public String getCron(String key) {
-        return crontabDao.getCron(key);
+        String result = (String) CacheManager.getInstance().get(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key)); // 先读取缓存
+        if (result == null) { // double check
+            synchronized (this) {
+                result = (String) CacheManager.getInstance().get(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key)); // 再次从缓存中读取，防止高并发情况下缓存穿透问题
+                if (result == null) { // 缓存中没有，再从数据库中读取，并写入缓存
+                    result = crontabDao.getCron(key);
+                    CacheManager.getInstance().put(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key), result);
+                }
+            }
+        }
+        return result;
     }
 
 }
