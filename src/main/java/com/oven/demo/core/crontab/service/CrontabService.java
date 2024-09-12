2dfccc907d626ecdcd7760bac0ad44d59ca37f9b
Oven <503612012@qq.com>
2024-09-12 19:06:39 +0800

    init

--- src/main/java/com/oven/demo/core/crontab/service/CrontabService.java
+++ src/main/java/com/oven/demo/core/crontab/service/CrontabService.java
@@ -1,10 +1,11 @@
 package com.oven.demo.core.crontab.service;
 
 import cn.hutool.core.util.StrUtil;
-import com.oven.basic.common.cache.CacheManager;
 import com.oven.demo.common.constant.CacheKeyConst;
+import com.oven.demo.common.redis.IRedisService;
 import com.oven.demo.core.crontab.dao.CrontabDao;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 
@@ -18,18 +19,20 @@ public class CrontabService {
 
     @Resource
     private CrontabDao crontabDao;
+    @Resource
+    private IRedisService redisService;
 
     /**
      * 根据key获取cron表达式
      */
     public String getCron(String key) {
-        String result = (String) CacheManager.getInstance().get(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key)); // 先读取缓存
-        if (result == null) { // double check
+        String result = redisService.getString(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key)); // 先读取缓存
+        if (StringUtils.isEmpty(result)) { // double check
             synchronized (this) {
-                result = (String) CacheManager.getInstance().get(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key)); // 再次从缓存中读取，防止高并发情况下缓存穿透问题
+                result = redisService.get(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key)); // 再次从缓存中读取，防止高并发情况下缓存穿透问题
                 if (result == null) { // 缓存中没有，再从数据库中读取，并写入缓存
                     result = crontabDao.getCron(key);
-                    CacheManager.getInstance().put(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key), result);
+                    redisService.setString(StrUtil.format(CacheKeyConst.GET_CRONTAB_BY_KEY, key), result, 86400);
                 }
             }
         }
